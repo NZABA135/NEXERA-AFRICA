7acfb16b2597a53a9f256073c4a5bda8f643115a
NZABANTERURA Jean Damascene <nzabajd135@gmail.com>
2026-04-24 17:52:16 +0200

    Delete unused file

--- includes/footer.php
+++
@@ -1,7 +0,0 @@
-    </main>
-    <footer>
-        <p>© 2026 NEXERA AFRICA Developed by INNOVAGENZ TECHNOLOGIES Ltd</p>
-    </footer>
-    <script src="assets/js/script.js"></script>
-</body>
-</html>
